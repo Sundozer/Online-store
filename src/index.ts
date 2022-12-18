22c25f9d17f4054a93c600726e6516318f8a412d
Alex Hramovich <xtr1m@mail.ru>
2022-12-18 15:11:51 +0300

    feat: half of work with checked checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,47 @@
 import './style.scss';
 import data from './data';
-
 const main = document.querySelector('.main');
+const asideBlock = document.querySelector('.aside-block');
+const activeFilters = {
+  category: undefined,
+  brand: undefined,
+  price: undefined,
+  stock: undefined
+};
 
-// function filter (array: object) {
-
-// }
-// filter(data)
-(function category () {
-  let arr: string[] = []
-  let array: string[] = []
-  let categories = document.querySelector('.aside-block_item-types')!;
+(function category() {
+  const arr: string[] = [];
+  const array: string[] = [];
+  const categories = document.querySelector('.aside-block_item-types')!;
   categories.innerHTML = '';
-  let input = document.createElement('checkbox');
-  let div = document.createElement('div');
-  div.classList.add('aside-block_item-category')
-  let divv = document.createElement('div');
-  divv.classList.add('aside-block_item-brands')
-  for (let i of data.products) {
+  const input = document.createElement('checkbox');
+  const div = document.createElement('div');
+  div.classList.add('aside-block_item-category');
+  const divv = document.createElement('div');
+  divv.classList.add('aside-block_item-brands');
+  /* eslint-disable-next-line */
+  for (const i of data.products) {
     if (!arr.includes(i.category)) {
-      
-      arr.push(i.category)
-      document.querySelector('.aside-block_item-types')!.appendChild(div)
+      arr.push(i.category);
+      document.querySelector('.aside-block_item-types')!.appendChild(div);
       document.querySelector('.aside-block_item-category')!.innerHTML += `<input type="checkbox" id="${i.category}" name="${i.category}"><span class="aside-block_one-of-items">${i.category}</span><br>`;
     }
     if (!array.includes(i.brand)) {
-      
-      array.push(i.brand)
-      document.querySelector('.aside-block_item-marks')!.appendChild(divv)
+      array.push(i.brand);
+      document.querySelector('.aside-block_item-marks')!.appendChild(divv);
       document.querySelector('.aside-block_item-brands')!.innerHTML += `<input type="checkbox" id="${i.brand}" name="${i.brand}"><span class="aside-block_one-of-items">${i.brand}</span><br>`;
     }
-    
   }
-})();
+}());
 
-
-
-
-
-
-
-
-
-
-
-// if (main !== null) {
-//     let o = 0;
-//     for (let i of data.products) {
-//         let img = document.createElement("img");
-//         img.src = data.products[o].images[0];
-//         img.classList.add('item-image');
-//         main.appendChild(img);
-//         o++;
-//     }
-// }
+asideBlock!.addEventListener('click', function(event){
+  const e = event.target as HTMLElement
+  if (e.tagName === 'SPAN') {
+    let checkbox = document.getElementById(`${e.id}`) as HTMLInputElement;
+    checkbox.checked = true;
+  }
+  if (e.tagName === 'INPUT') {
+    console.log(e.id)
+  }
+  
+})
