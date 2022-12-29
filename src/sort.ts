28b78b07345ffb13fcda3de9e3504ef89c1f7916
Berkut4713 <av.vecherov@gmail.com>
2022-12-29 19:22:48 +0400

    feat: add sort price ASC and DESC

--- src/sort.ts
+++ src/sort.ts
@@ -1,18 +1,51 @@
 import data from './data';
 import { IFilteredData } from './interfaces';
 
-export function sortDate(objectCardsFilter: IFilteredData[]/*, property: string | number*/) {
-  objectCardsFilter.sort((a, b) => {
-    return a.price - b.price;
-});
-//   const sortedArray: { property: string | number; }[] = objectCardsFilter.sort((n1,n2) => {
-//     if (n1.property > n2.property) {
-//         return 1;
-//     }
+export function sortDate(value: string, objectCardsFilter: IFilteredData[]) {
+  // debugger;
+  switch(value) {
+    case 'price ASC':
+      sortDatePriceASC(objectCardsFilter);
+      break;
+    case 'price DESC':
+      sortDatePriceDESC(objectCardsFilter);
+      break;
+    default:
+      break;
+    }
+  }
 
-//     if (n1.property < n2.property) {
-//         return -1;
-//     }
-//     return 0;
+
+  function sortDatePriceASC(objectCardsFilter: IFilteredData[]) {
+    objectCardsFilter.sort((a, b) => {
+      return a.price - b.price;
+    });
+  }
+
+  function sortDatePriceDESC(objectCardsFilter: IFilteredData[]) {
+    objectCardsFilter.sort((a, b) => {
+      return b.price - a.price;
+    });
+  }
+  
+
+
+// const optionElements = document.querySelectorAll('.options');
+// optionElements[0]!.addEventListener('click', (event) => {
+//   const e = event.target as HTMLElement;
+//   // if (e === option)
+//   debugger;
+// })
+
+// var sortedArray: { age: number; }[] = objectArray.sort((n1, n2) => {
+//   if (n1.age > n2.age) {
+//       return 1;
+//   }
+
+//   if (n1.age < n2.age) {
+//       return -1;
+//   }
+
+//   return 0;
 // });
-}
+
