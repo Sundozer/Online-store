89be3b3f770a23897523527aea6a0c0937e48129
Alex Hramovich <xtr1m@mail.ru>
2023-01-08 18:08:33 +0300

    feat: add local svg

--- src/index.ts
+++ src/index.ts
@@ -174,6 +174,10 @@ function checkURL(e?: string) { // чекает юрл, чтобы заполн�
 }
 
 (function category() { // заполняет блоки элементами из даты
+  /* eslint-disable-next-line */
+  const logo = require('./assets/rsschool.svg') as string;
+  const fff = document.querySelector('.svg') as HTMLImageElement;
+  fff.src = logo;
   const arr: string[] = [];
   const array: string[] = [];
   const categories = document.querySelector('.aside-block_item-types')!;
